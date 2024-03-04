f96da1d29754103ef8037f42eb14374aada25118
geyu <geyu210@gmail.com>
2024-03-04 15:11:44 +0800

    Update multicall_send_eth.ts with new RPC URL and contract addresses

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # starknet.js-examples
 
+安装 >18.0.0 node https://nodejs.org/en
+
+```
+npm install --global yarn
+
+git clone https://github.com/Starknet-ZH/starknet.js-examples.git
+
+cd starknet.js-examples
+
 yarn install
 
-yarn ts-node src/yourpath.ts
+yarn ts-node src/multicall_send_eth.ts
 
 将 env_example 文件名修改为 .env，然后在 privatekey= 后填入argent钱包导出的私钥，不建议导出有大额资金或者可能获得空投的账号私钥
 
+
+```
--- src/multicall_send_eth.ts
+++ src/multicall_send_eth.ts
@@ -3,12 +3,13 @@
     import { log } from "console";
     import { cairo } from "starknet";
     dotenv.config();
-    const privateKey = process.env.privatekey;
-    const provider = new RpcProvider({ nodeUrl:'https://free-rpc.nethermind.io/sepolia-juno/v0_5'});
+    const privateKey = process.env.privatekey; // 导入私钥
+    const provider = new RpcProvider({ nodeUrl:'https://free-rpc.nethermind.io/sepolia-juno/v0_5'}); //rpc url
     const privateKeyAX = privateKey!;
     const AXcontractAddress = "0x0672a8021A17B9137e57d4691893329203967c85E8178ce4f2E65D898E623dC5"; // 替换为你的钱包地址
     const accountAX_new  = new Account(provider, AXcontractAddress, privateKeyAX, '1');
     console.log(accountAX_new.address);
+
     async function main(){
         const ethAddress = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7" //eth contract address
         // read abi of Test contract
@@ -30,10 +31,18 @@
             entrypoint: 'transfer',
             calldata: CallData.compile({
                 recipient:"0x03E64bb02F1Bf09825079ed786A827995F386734F1EDfA5fBd4Da169979C015a", //接收者地址
-                amount: cairo.uint256(100000000000000000n),
+                amount: cairo.uint256(100000000000000000n), //0.1 ETH
             }),
             },
             // // Calling the second contract
+            {
+                contractAddress: ethAddress,
+                entrypoint: 'transfer',
+                calldata: CallData.compile({
+                    recipient:"0x03E64bb02F1Bf09825079ed786A827995F386734F1EDfA5fBd4Da169979C015a",//接收者地址
+                    amount: cairo.uint256(100000000000000000n),
+                }),
+            },
             {
                 contractAddress: ethAddress,
                 entrypoint: 'transfer',
